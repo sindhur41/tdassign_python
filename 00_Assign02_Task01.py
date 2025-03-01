efc794a0545a440861b13cd5667ed78078eff1a3
sindhur41 <sindhur41@gmail.com>
2025-03-01 14:36:13 +0530

    Add files via upload
    Assignment two of the tutedude python program. Includes both task1 and 2.
    00_Assign02_Task01- Program to find even or odd number
    00_Assign02_Task02-Program to find sum of 1to 50

---
+++ 00_Assign02_Task01.py
@@ -0,0 +1,9 @@
+#OddorEven
+a=int(input('Enter an integer ='))
+b=a%2
+if (b==0):
+    print (a, ' is an even number')
+else :
+    print (a, ' is an odd number')
+
+print ('Thank you')
---
+++ 00_Assign02_Task02.py
@@ -0,0 +1,6 @@
+#SumOfintegers1to50usingLoop
+a=0
+for i in range(1,50) :
+    a=a+i
+    i=i+1
+print ('sum of numbers from 1 to 50 is', a)
