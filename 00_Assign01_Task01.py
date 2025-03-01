be7f18f2d24ddca01da7d168380e221c826bcaa8
sindhur41 <sindhur41@gmail.com>
2025-03-01 13:37:51 +0530

    Add files via upload
    Assignments of tutedude python course

---
+++ 00_Assign01_Task01.py
@@ -0,0 +1,16 @@
+#Mathematical Operation of two numbers
+
+a= float(input ('Enter the first number = '))
+b= float(input ('Enter the second number = '))
+
+#Addition
+O1=a+b
+#Substraction
+O2=a-b
+#Multiplication
+O3=a*b
+#Division
+O4=a/b
+print(f'Addition = {O1} \nSubstraction = {O2}')
+print(f'Multiplication = {O3}\nDivision= {O4}')
+print('Thank You')
---
+++ 00_Assign01_Task02.py
@@ -0,0 +1,5 @@
+#Greeting
+a= input ('Enter user\'s first name - ')
+b= input ('Enter user\'s last name - ')
+c=a+" "+b
+print ('Hello, ' +c+'! welcome to the python program.')
